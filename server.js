03587d30adf75dfda02f07eb937a9090fc869f65
Trentyn <63451568+Trentyn@users.noreply.github.com>
2025-10-03 12:21:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -9,15 +8,33 @@ const io = new Server(server);
 // Serve static files (our frontend)
 app.use(express.static("public"));
 
+const usernames = new Set(); // Track active usernames
+
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
-  socket.on("chat message", (data) => {
-    console.log(`${data.user}: ${data.text}`);
-    io.emit("chat message", data); // broadcast name + message
+  // Handle join request
+  socket.on("join", (username) => {
+    if (usernames.has(username)) {
+      socket.emit("username taken");
+    } else {
+      usernames.add(username);
+      socket.username = username; // Store username on socket
+      socket.emit("joined");
+    }
   });
 
+  // Handle chat messages
+  socket.on("chat message", (data) => {
+    console.log(`${data.user}: ${data.text}`);
+    io.emit("chat message", data); // Broadcast name + message
+  });
+
+  // Handle disconnect
   socket.on("disconnect", () => {
+    if (socket.username) {
+      usernames.delete(socket.username); // Free the username
+    }
     console.log("User disconnected:", socket.id);
   });
 });
