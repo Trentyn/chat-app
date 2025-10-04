c3f977a6131e1066478b8a73323e95239284ee2d
Trentyn <63451568+Trentyn@users.noreply.github.com>
2025-10-05 00:24:05 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,42 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { 
-    type: String, 
-    required: true, 
-    unique: true 
-  },
-  secret: { 
-    type: String, 
-    required: true 
-  },
-  recoveryKey: { 
-    type: String, 
-    required: true 
-  },
-  verified: { 
-    type: Boolean, 
-    default: false 
-  }
+  username: { type: String, required: true, unique: true },
+  secret: { type: String, required: true },
+  recoveryKey: { type: String, required: true }
 });
 
 const messageSchema = new mongoose.Schema({
-  type: { 
-    type: String, 
-    required: true 
-  },
-  user: { 
-    type: String, 
-    required: true 
-  },
+  type: { type: String, required: true },
+  user: { type: String, required: true },
   text: String,
-  timestamp: { 
-    type: Date, 
-    default: Date.now 
-  }
+  timestamp: { type: Date, default: Date.now }
 });
 
-const User = mongoose.model('User', userSchema);
-const Message = mongoose.model('Message', messageSchema);
-
-module.exports = { User, Message };
+exports.User = mongoose.model("User", userSchema);
+exports.Message = mongoose.model("Message", messageSchema);
