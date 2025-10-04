81209a085be4894baaf57ffc18d4117ecf01185a
Trentyn <63451568+Trentyn@users.noreply.github.com>
2025-10-05 00:50:07 +0100

    Update and rename User.js to user.js

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   secret: { type: String, required: true },
-  recoveryKey: { type: String, required: true }
+  recoveryKey: { type: String, required: true },
+  theme: { type: String, enum: ["light", "dark"], default: "light" }
 });
 
 const messageSchema = new mongoose.Schema({
